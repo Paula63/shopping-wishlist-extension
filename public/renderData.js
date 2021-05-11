dad0f4ed121198673d72bfbc76fe852c7cb3d06e
Paula63 <x17370023@student.ncirl.ie>
2021-05-11 15:10:47 +0100

    rendering data from firebase in console for testing

---
+++ public/renderData.js
@@ -0,0 +1,12 @@
+db.collection('wishboards').get().then((snapshot) => {
+    // snapshot.docs.forEach(doc => {
+    //     renderWishboards(doc);
+    // })
+    console.log("wishboard here", snapshot.docs);
+})
+
+db.collection('wishboards').get().then((snapshot) => {
+    snapshot.docs.forEach(doc => {
+        console.log("doc", doc.data());
+    });
+})
